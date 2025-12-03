8d584f02f3aefb1e5f10701fc4952959e0ed9b63
Eryc-cmd <cebolinha4703c@gmail.com>
2025-12-03 01:04:47 +0000

    fase 1

--- xadrez.c
+++ xadrez.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    char Estado[50];
+    char Nome da cidade[50];
+    char Código da carta[50];
+    int População;
+    int Numeros de pontos turisticos;
+    float Área em km;
+    float PIB;
 
     printf("Desafio xadrez:\n");
-
+    scanf("")
 
 
 }
